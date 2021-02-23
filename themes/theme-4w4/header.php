afc004ceb3698b8b4a425f6482e3d8e4dba95a2d
Finder-00 <you@example.com>
2021-02-23 16:15:32 -0500

    ajout du carrousel

--- themes/theme-4w4/header.php
+++ themes/theme-4w4/header.php
@@ -45,6 +45,21 @@
 			<?php endif; ?>
 		</div><!-- .site-branding -->
 
+		<!-- debut du carrousel -->
+		<?php 
+		if( is_front_page()) : ?>
+		<section class="carrousel">
+			<div>1</div>
+			<div>2</div>
+			<div>3</div>
+		</section>
+			<button id="un">1</button>
+			<button id="deux">2</button>
+			<button id="trois">3</button>
+		<?php endif; ?>
+		<!-- fin carrousel -->
+
+
 		<nav id="site-navigation" class="main-navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><img src="https://s2.svgbox.net/hero-outline.svg?ic=view-list&color=000000" width="32" height="32"></button>
 			<?php
